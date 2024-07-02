a064a742cfe06b9febfd2c0e706c0dd68564e693
hanbyul0725 <s2234@e-mirim.hs.kr>
2024-07-03 08:54:02 +0900

    #6 23-7 섀도잉

---
+++ CSProject2/program.cs
@@ -0,0 +1,13 @@
+namespace CSPractice2
+{
+
+    internal class Program
+    {
+        int number = 10;
+        static void Main(string[] args)
+        {
+            // #6 23-7 ¼¨µµÀ×
+            int number = 20;
+            Console.WriteLine(number); // 20
+        }
+    }
