b8f2bf6dd53a7ece13343e046806a964778ac92f
hanbyul0725 <s2234@e-mirim.hs.kr>
2024-07-03 09:10:49 +0900

    #6 23-7 하이딩

--- CSProject2/program.cs
+++ CSProject2/program.cs
@@ -9,5 +9,18 @@ namespace CSPractice2
             // #6 23-7 섀도잉
             int number = 20;
             Console.WriteLine(number); // 20
+
+            // #6 23-7 하이딩
+            Child child = new Child();
+            Console.WriteLine(child.variable); // 출력: 하이딩
+            child.Method(); // 출력: 자식메서드
+            Console.WriteLine(((Parent)child).variable); // 출력: 273
+            ((Parent)child).Method(); // 출력: 부모 메서드
+            Parent p = child;
+            Console.WriteLine(p.variable); // 출력: 273
+
         }
+
     }
+
+}
